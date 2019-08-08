fdb3f2d9ed3d0129588feba6a970e429f43a56ef
Khoirur-Rozikin <krozikin310@gmail.com>
2019-08-08 21:37:05 +0700

    tugas_pbo

---
+++ kendaraan.php
@@ -0,0 +1,56 @@
+<?php 
+class motor{
+	var $merk;
+	var $transportasi;
+	var $bahan_bakar;
+
+	public function setmerk($merk=''){
+		$this->merk=$merk;
+	}
+	public function settransportasi($transportasi=''){
+		$this->transportasi=$transportasi;
+	}
+	public function setbahan_bakar($bahan_bakar=''){
+		$this->bahan_bakar=$bahan_bakar;
+	}
+	public function cetak(){
+		echo'merk :'.$this->merk.",";
+		echo'transportasi :'.$this->transportasi.",";
+		echo'bahan_bakar :'.$this->bahan_bakar.",";;
+	}
+}
+$motor = new motor();
+$motor->setmerk('mio j');
+$motor->settransportasi('darat');
+$motor->setbahan_bakar('bensin');
+$motor->cetak();
+echo "<br>";
+
+$motor = new motor();
+$motor->setmerk('kawasaki');
+$motor->settransportasi('darat');
+$motor->setbahan_bakar('pertamax');
+$motor->cetak();
+echo "<br>";
+
+$motor = new motor();
+$motor->setmerk('supra');
+$motor->settransportasi('darat');
+$motor->setbahan_bakar('bensin');
+$motor->cetak();
+echo "<br>";
+
+$motor = new motor();
+$motor->setmerk('vario 150');
+$motor->settransportasi('darat');
+$motor->setbahan_bakar('bensin');
+$motor->cetak();
+echo "<br>";
+
+$motor = new motor();
+$motor->setmerk('satria fu');
+$motor->settransportasi('darat');
+$motor->setbahan_bakar('pertamax');
+$motor->cetak();
+echo "<br>";
+
